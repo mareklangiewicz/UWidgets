2cd81af6e2502b1237b0c7609e3a02b4d3344faf
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-08-22 13:11:39 +0200

    fun

--- udemo/src/commonMain/kotlin/udemo/UDemos.ski.kt
+++ udemo/src/commonMain/kotlin/udemo/UDemos.ski.kt
@@ -1,8 +1,12 @@
 package pl.mareklangiewicz.udemo
 
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.unit.*
+import kotlinx.coroutines.*
 import pl.mareklangiewicz.playgrounds.*
 import pl.mareklangiewicz.uwidgets.*
 import pl.mareklangiewicz.uwidgets.UContainerType.*
@@ -21,5 +25,21 @@ import pl.mareklangiewicz.uwidgets.UContainerType.*
             }
         }
         UReportsUi(reportsModel)
+    }
+}
+
+@Composable fun UDemoMoveStuffSki() {
+    var count by remember { mutableStateOf(0) }
+    LaunchedEffect(Unit) {
+        while (isActive) {
+            delay(300)
+            count++
+        }
+    }
+    val countf by animateFloatAsState(count.toFloat())
+    // Some crazy moving layouts to stress test layout/snapshot system inside browser canvas environment
+    Column(Modifier.padding(horizontal = (countf * 5).dp)) {
+        UDemo2(DpSize((100 + countf * 3).dp, (300 - countf * 4).dp))
+        for (i in 1..6) Text("x".repeat(i), fontSize = (countf * 2 + 16 - i).sp)
     }
 }
--- udemo/src/jsMain/kotlin/act/UDemo3.js.kt
+++ udemo/src/jsMain/kotlin/act/UDemo3.js.kt
@@ -1,12 +1,7 @@
 package pl.mareklangiewicz.udemo
 
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.*
 import androidx.compose.ui.unit.*
-import kotlinx.coroutines.*
 import org.jetbrains.compose.web.attributes.*
 import pl.mareklangiewicz.usystem.*
 
@@ -16,22 +11,10 @@ import pl.mareklangiewicz.usystem.*
     withVerticalScroll: Boolean,
 ) {
     UCanvasWindow(attrs = {
-        width(size.width.value.toInt())
-        height(size.height.value.toInt())
+        width(size.width.value.toInt() * 3)
+        height(size.height.value.toInt() * 2)
     }) {
-        var count by remember { mutableStateOf(0) }
-        LaunchedEffect(Unit) {
-            while (isActive) {
-                delay(300)
-                count ++
-            }
-        }
-        val countf by animateFloatAsState(count.toFloat())
-        // Some crazy moving layouts to stress test layout/snapshot system inside browser canvas environment
-        Column(Modifier.padding(horizontal = (countf * 5).dp)) {
-            Text("Canvas DEMO DOM")
-            UDemo2(DpSize((100 + countf*3).dp, (300 - countf*4).dp))
-            for (i in 1..6) Text("x".repeat(i), fontSize = (countf*2 + 16 - i).sp)
-        }
+        // UDemo3ImplSki(size, true, true)
+        UDemoMoveStuffSki()
     }
 }
