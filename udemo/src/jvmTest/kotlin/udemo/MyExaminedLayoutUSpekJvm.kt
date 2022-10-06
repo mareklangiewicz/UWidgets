02a1ce56fc13ca42e059f42ce5eebf7d3a2d8693
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-10-06 17:56:21 +0200

    fun

--- udemo/src/jvmTest/kotlin/udemo/MyExaminedLayoutUSpekJvm.kt
+++ udemo/src/jvmTest/kotlin/udemo/MyExaminedLayoutUSpekJvm.kt
@@ -1,12 +1,14 @@
 package pl.mareklangiewicz.udemo
 
 import androidx.compose.ui.test.junit4.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.test.*
 import org.junit.*
 import org.junit.runner.*
 import pl.mareklangiewicz.ulog.*
 import pl.mareklangiewicz.uspek.*
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(USpekJUnit4Runner::class)
 class MyExaminedLayoutUSpekJvm {
     init {
@@ -16,6 +18,5 @@ class MyExaminedLayoutUSpekJvm {
     @get:Rule val rule = createComposeRule()
     private val scope = UComposeRuleScope(rule)
 
-    @Suppress("MISSING_DEPENDENCY_CLASS") // IDE issue
     @USpekTestTree(33) fun melusf() = runTest { scope.MyExaminedLayoutUSpekFun() }
 }
