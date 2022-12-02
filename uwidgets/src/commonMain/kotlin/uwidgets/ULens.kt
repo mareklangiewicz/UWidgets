6e30c2f7bb91770ab6ae6f51885bb548042114d1
Marek Langiewicz <marek.langiewicz@gmail.com>
2022-12-02 12:27:32 +0100

    fun

--- uwidgets/src/commonMain/kotlin/uwidgets/ULens.kt
+++ uwidgets/src/commonMain/kotlin/uwidgets/ULens.kt
@@ -13,19 +13,20 @@ import androidx.compose.ui.Modifier as Mod
 
 @OptIn(ExperimentalFoundationApi::class)
 fun Mod.ulens(zoom: Float = 1f) = composed { when {
-    !MagnifierStyle.Default.isSupported -> this.also { ulogw("Magnifier is not supported on this platform.") }
-    zoom == 1f -> this
-    else -> {
-        var center by ustate(Offset.Unspecified)
-        magnifier({ center }, zoom = zoom).pointerInput(Unit) { detectDragGestures(
-            // Show the magnifier at the original pointer position.
-            onDragStart = { center = it },
-            // Make the magnifier follow the finger while dragging.
-            onDrag = { _, delta -> center += delta },
-            // Hide the magnifier when the finger lifts.
-            onDragEnd = { center = Offset.Unspecified },
-            onDragCancel = { center = Offset.Unspecified }
-        ) }
-    }
+    else -> this.also { ulogw("Disabled. TODO: enable when MagnifierStyle symbol itself is available in compose-jb") }
+    // !MagnifierStyle.Default.isSupported -> this.also { ulogw("Magnifier is not supported on this platform.") }
+    // zoom == 1f -> this
+    // else -> {
+    //     var center by ustate(Offset.Unspecified)
+    //     magnifier({ center }, zoom = zoom).pointerInput(Unit) { detectDragGestures(
+    //         // Show the magnifier at the original pointer position.
+    //         onDragStart = { center = it },
+    //         // Make the magnifier follow the finger while dragging.
+    //         onDrag = { _, delta -> center += delta },
+    //         // Hide the magnifier when the finger lifts.
+    //         onDragEnd = { center = Offset.Unspecified },
+    //         onDragCancel = { center = Offset.Unspecified }
+    //     ) }
+    // }
 } }
 
