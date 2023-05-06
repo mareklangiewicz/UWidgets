49ec5e2e526a6a8a2474ab20bc53a561dce4ea8a
Marek Langiewicz <marek.langiewicz@gmail.com>
2023-05-06 20:18:25 +0200

    fun

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@
 // gradle.logSomeEventsToFile(rootProject.projectDir.toOkioPath() / "my.gradle.log")
 
 pluginManagement {
-    includeBuild("../DepsKt")
+//    includeBuild("../DepsKt")
     repositories {
         mavenLocal()
         google()
@@ -15,7 +15,19 @@ pluginManagement {
     }
 }
 
-plugins { id("pl.mareklangiewicz.deps.settings") version "0.2.34" }
+plugins {
+    id("pl.mareklangiewicz.deps.settings") version "0.2.36" // https://plugins.gradle.org/search?term=mareklangiewicz
+    id("com.gradle.enterprise") version "3.13.1" // https://docs.gradle.com/enterprise/gradle-plugin/
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlwaysIf(System.getenv("GITHUB_ACTIONS") == "true")
+        publishOnFailure()
+    }
+}
 
 rootProject.name = "UWidgets"
 
