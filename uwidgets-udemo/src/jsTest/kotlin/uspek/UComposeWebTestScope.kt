c27e24ce5b213600ee538a062735beecfeb24950
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-06-10 16:50:01 +0200

    fun

--- uwidgets-udemo/src/jsTest/kotlin/uspek/UComposeWebTestScope.kt
+++ uwidgets-udemo/src/jsTest/kotlin/uspek/UComposeWebTestScope.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.unit.*
 import org.jetbrains.compose.web.testutils.*
 import pl.mareklangiewicz.udata.*
+import pl.mareklangiewicz.ulog.hack.ulog
 import pl.mareklangiewicz.ulog.*
 import pl.mareklangiewicz.uwidgets.*
 
@@ -11,7 +12,7 @@ import pl.mareklangiewicz.uwidgets.*
 @OptIn(ComposeWebExperimentalTestsApi::class)
 class UComposeWebTestScope(
   private val webTestScope: TestScope,
-  log: (Any?) -> Unit = { ulogd(it.ustr) },
+  log: (Any?) -> Unit = { ulog.d(it.ustr) },
 ) : UComposeScope {
   override fun setContent(composable: @Composable () -> Unit) = webTestScope.composition(composable)
   override suspend fun awaitIdle() = webTestScope.waitForRecompositionComplete()
