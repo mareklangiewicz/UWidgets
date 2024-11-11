95a317932d43cadb304a4d5c151d183723b96b0b
Marek Langiewicz <marek.langiewicz@gmail.com>
2024-11-11 18:21:46 +0100

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ defaultBuildTemplateForRootProject(
     name = "UWidgets",
     description = "Micro widgets for Compose Multiplatform",
     githubUrl = "https://github.com/mareklangiewicz/UWidgets",
-    version = Ver(0, 0, 32),
+    version = Ver(0, 0, 33),
     settings = LibSettings(
       withJvm = enableJvm,
       withJs = enableJs,
