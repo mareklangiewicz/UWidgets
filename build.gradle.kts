393798d88acb731958829316e9b3742a7b7f071c
Marek Langiewicz <marek.langiewicz@gmail.com>
2025-01-31 20:55:49 +0100

    fun

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ val enablePublishing = findProject(":kground") == null
 // (see settings.gradle.kts) so it would also publish these with wrong description and ver etc.
 // exception: publishToMavenLocal for debugging
 
-rootExtString["verKGround"] = "0.1.03" // https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/kground/
+rootExtString["verKGround"] = "0.1.05" // https://s01.oss.sonatype.org/content/repositories/releases/pl/mareklangiewicz/kground/
 
 
 defaultBuildTemplateForRootProject(
